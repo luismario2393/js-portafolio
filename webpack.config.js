8c23689a6f03aa0f0430d5bcb8470b8c901366c5
LUIS MARIO AMOR SANCHEZ <65006606+luismario2393@users.noreply.github.com>
2021-06-28 15:20:11 -0500

    variables de entorno webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
     },
     module: {
         rules: [
@@ -40,9 +49,9 @@ module.exports = {
                     options: {
                         limit: 10000,
                         mimetype: "aplication/font-woff",
-                        name: "[name].[ext]",
+                        name: "[name].[contenthash].[ext]",
                         outputPath: "./assets/fonts",
-                        publicPath: "./assets/fonts/",
+                        publicPath: "../assets/fonts/",
                         esModule: false,
                     },
                 }
@@ -55,7 +64,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
         new CopyPlugin({
             patterns: [
                 {
@@ -63,8 +74,16 @@ module.exports = {
                     to: "assets/images"
                 }
             ]
-        })
-    ]
+        }),
+        new Dotenv(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
     
 
 }
