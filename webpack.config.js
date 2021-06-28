5f032d8c3202ae4ee1fd7307c61b13167a3395a9
LUIS MARIO AMOR SANCHEZ <65006606+luismario2393@users.noreply.github.com>
2021-06-28 10:31:52 -0500

    vamos por html en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -20,6 +21,13 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
     
 
 }
